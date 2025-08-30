82e2194b327a412efcbd8b6fb83bd659e8246408
Markus Hofmann <m.hofmann@web-vision.de>
2025-08-30 23:31:15 +0200

    [TASK] Refactor `HistoryRecord` to `AbstractEntityEventListener`
    The original idea was to make it possible to use the trait in other
    Classes beside to the eventListeners. As this makes no sense at the
    moment, move the functionality to an abstract class and the single
    functionality to thhe respective EventListeners.
    
    As an extension of EventListeners is not useful, make them final.
    
    Add DocBlocks to the classes describing their use case.
    
    Remove the `RecordHistoryStore`, which was a copy of the original one
    from the core, as this could handle our issues, too.

--- Classes/EventListener/EntityNewListener.php
+++ Classes/EventListener/EntityNewListener.php
@@ -1,33 +1,52 @@
 <?php
 
-/**
- * Markus Hofmann
- * 13.10.21 11:29
- */
-
 declare(strict_types=1);
 
 namespace SUDHAUS7\FeDataHistory\EventListener;
 
 use SUDHAUS7\FeDataHistory\Domain\HistoryEntityInterface;
-use SUDHAUS7\FeDataHistory\Traits\HistoryRecord;
+use TYPO3\CMS\Core\Context\Exception\AspectNotFoundException;
+use TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface;
 use TYPO3\CMS\Extbase\Event\Persistence\EntityFinalizedAfterPersistenceEvent;
+use TYPO3\CMS\Extbase\Persistence\Generic\Exception;
 
-class EntityNewListener
+/**
+ * This event listener adds a sys_history record, if the given Extbase model
+ * is added via frontend and has the {@see HistoryEntityInterface} added.
+ * @see EntityFinalizedAfterPersistenceEvent
+ */
+final class EntityNewListener extends AbstractEntityEventListener
 {
-    use HistoryRecord;
-
     /**
      * __invoke
      * @param EntityFinalizedAfterPersistenceEvent $event
-     * @throws \TYPO3\CMS\Core\Context\Exception\AspectNotFoundException
-     * @throws \TYPO3\CMS\Extbase\Persistence\Generic\Exception
+     * @throws AspectNotFoundException
+     * @throws Exception
      */
     public function __invoke(EntityFinalizedAfterPersistenceEvent $event): void
     {
         $object = $event->getObject();
         if ($object instanceof HistoryEntityInterface) {
             $this->writeNew($object);
+        }
+    }
+
+    /**
+     * @param DomainObjectInterface $object
+     * @throws Exception|AspectNotFoundException
+     */
+    protected function writeNew(DomainObjectInterface $object): void
+    {
+        $newRecord = array_map(function ($value) {
+            return $value;
+        }, $object->_getProperties());
+
+        if (count($newRecord) > 0) {
+            $tableName = $this->getTableName($object);
+
+            if (!empty($tableName)) {
+                $this->getRecordHistoryStore()->addRecord($tableName, (int)$object->getUid(), ['newRecord' => $newRecord]);
+            }
         }
     }
 }
--- Classes/EventListener/EntityUpdatedListener.php
+++ Classes/EventListener/EntityUpdatedListener.php
@@ -1,25 +1,26 @@
 <?php
 
-/**
- * Markus Hofmann
- * 13.10.21 11:15
- */
-
 declare(strict_types=1);
 
 namespace SUDHAUS7\FeDataHistory\EventListener;
 
 use SUDHAUS7\FeDataHistory\Domain\HistoryEntityInterface;
-use SUDHAUS7\FeDataHistory\Traits\HistoryRecord;
 use TYPO3\CMS\Core\Context\Exception\AspectNotFoundException;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\DomainObject\DomainObjectInterface;
 use TYPO3\CMS\Extbase\Event\Persistence\EntityUpdatedInPersistenceEvent;
 use TYPO3\CMS\Extbase\Persistence\Generic\Exception;
 use TYPO3\CMS\Extbase\Persistence\Generic\Exception\TooDirtyException;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
-class EntityUpdatedListener
+/**
+ * With this EventListener a sys_history entry is added when a frontend user changes
+ * an Extbase entity.
+ * @see EntityUpdatedInPersistenceEvent
+ */
+final class EntityUpdatedListener extends AbstractEntityEventListener
 {
-    use HistoryRecord;
-
     /**
      * __invoke
      * @param EntityUpdatedInPersistenceEvent $event
@@ -33,6 +34,57 @@ class EntityUpdatedListener
         if ($object instanceof HistoryEntityInterface) {
             if ($object->_isDirty()) {
                 $this->writeModified($object);
+            }
+        }
+    }
+
+    /**
+     * @param DomainObjectInterface $object
+     * @throws TooDirtyException
+     * @throws Exception|AspectNotFoundException
+     */
+    protected function writeModified(DomainObjectInterface $object): void
+    {
+        $oldRecord = [];
+        $newRecord = [];
+        foreach ($object->_getProperties() as $property => $value) {
+            if ($object->_isDirty($property)) {
+                $dbProperty = $this->getDbFieldName($property, $object);
+                if ($value instanceof ObjectStorage) {
+                    $oldValue = [];
+                    /** @var AbstractEntity $cleanObject */
+                    foreach ($object->_getCleanProperty($property) as $cleanObject) {
+                        $oldValue[] = $cleanObject->getUid();
+                    }
+                    $newValue = [];
+                    /** @var AbstractEntity $dirtyObject */
+                    foreach ($value as $dirtyObject) {
+                        $newValue[] = sprintf('%s_%s', $this->getTableName($dirtyObject), $dirtyObject->getUid());
+                    }
+                    $oldRecord[$dbProperty] = implode(',', $oldValue);
+                    $newRecord[$dbProperty] = implode(',', $newValue);
+                } elseif ($value instanceof FileReference) {
+                    $oldRecord[$dbProperty] = ($object->_getCleanProperty($property) ? $object->_getCleanProperty($property)->getUid() : null);
+                    $newRecord[$dbProperty] = $value->getUid();
+                } else {
+                    $oldRecord[$dbProperty] = $object->_getCleanProperty($property);
+                    $newRecord[$dbProperty] = $value;
+                }
+            }
+        }
+
+        if (count($oldRecord) > 0 && count($newRecord) > 0) {
+            $oldRecord['l10n_diffsource'] = serialize(
+                []
+            );
+            $newRecord['l10n_diffsource'] = serialize(
+                []
+            );
+
+            $tableName = $this->getTableName($object);
+
+            if (!empty($tableName)) {
+                $this->getRecordHistoryStore()->modifyRecord($tableName, (int)$object->getUid(), ['oldRecord' => $oldRecord, 'newRecord' => $newRecord]);
             }
         }
     }
